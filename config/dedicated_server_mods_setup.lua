3412126b1bb7a9e79f6976720698615f77b9317b
Jeroen Kruis <kruisjeroen@gmail.com>
2016-02-11 22:50:03 +0100

    Added configuration folder containing mods

---
+++ config/dedicated_server_mods_setup.lua
@@ -0,0 +1,5 @@
+-- Steam Workshop DST Mods
+ServerModSetup("378160973") -- Global Positions
+ServerModSetup("369588618") -- Renewable World
+ServerModSetup("374550642") -- Increased Stack size
+ServerModSetup("396026892") -- Large Chest
---
+++ config/modoverrides.lua
@@ -0,0 +1,6 @@
+return {
+	["workshop-378160973"] = { enabled = true },
+	["workshop-369588618"] = { enabled = true },
+	["workshop-374550642"] = { enabled = true },
+	["workshop-396026892"] = { enabled = true }
+}
